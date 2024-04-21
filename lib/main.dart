e17d60003048825eb81a88899ceda8bdc9dcf1f7
MSI\karly <marmol0karla@gmail.com>
2024-04-21 02:47:18 -0700

    - [x] Hacer el menú de navegación - [] Hacer el dart de la base de datos - [] Hacer los manejadores de datos de cada entidad - [] Hacer la GUI de los CRUD de materia - [] Programar los CRUDS de materia - [] Hacer la GUI de los CRUD de profesor - [] Programar los CRUDS de profesor - [] Hacer la GUI de los CRUD de Horario - [] Programar los CRUDS de Horario - [] Hacer la GUI de los CRUD de Asistencia - [] Programar los CRUDS de Asistencia
    Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp(),debugShowCheckedModeBanner: false
+    ,));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,8 +13,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  int _indice= 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CHECADOR",style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Montserrat',
+          color: Colors.white,
+          letterSpacing: 2.0,
+          shadows: [
+            Shadow(
+              color: Colors.black,
+              blurRadius: 2,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: dinamico(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.menu_book_sharp), label: "Materia" , backgroundColor: Colors.green),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: "Profesor" , backgroundColor: Colors.yellowAccent.shade700),
+          BottomNavigationBarItem(icon: Icon(Icons.access_time_filled), label: "Horario" , backgroundColor: Colors.red),
+          BottomNavigationBarItem(icon: Icon(Icons.format_list_numbered), label: "Asistencia" , backgroundColor: Colors.blue),
+        ],
+        currentIndex: _indice,
+        onTap: (index){
+          setState(() {
+            _indice=index;
+
+          });
+        },
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.white,
+      ),
+    );
   }
+
+  dinamico() {}
 }
