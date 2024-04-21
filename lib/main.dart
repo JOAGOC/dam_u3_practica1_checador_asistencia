6e2ed7a3224ddc0b89cb1c6a14743a4b2596d626
angel <joangonzalezcr@ittepic.edu.mx>
2024-04-20 23:16:24 -0700

    - [X] Hacer el menú de navegación - [-] Adaptar el Floating Action Button a los distintos menus. - [X] Hacer el dart de la base de datos - [-] Hacer los manejadores de datos de cada entidad - [-] Hacer la GUI de los CRUD de materia - [X] Programar los CRUDS de materia - [] Hacer la GUI de los CRUD de profesor - [] Programar los CRUDS de profesor - [] Hacer la GUI de los CRUD de Horario - [] Programar los CRUDS de Horario - [] Hacer la GUI de los CRUD de Asistencia - [] Programar los CRUDS de Asistencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,55 @@
+import 'package:dam_u3_practica1_checador_asistencia/Controlador/DBMateria.dart';
+import 'package:dam_u3_practica1_checador_asistencia/GUIMateria.dart';
+import 'package:dam_u3_practica1_checador_asistencia/Modelo/Materia.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp(),debugShowCheckedModeBanner: false
-    ,));
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
+  int _indice = 0;
+  List<Materia> materias = [];
+  var nmat = TextEditingController();
+  var descripcion = TextEditingController();
 
-  int _indice= 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    consultarMaterias();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("CHECADOR",style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Montserrat',
-          color: Colors.white,
-          letterSpacing: 2.0,
-          shadows: [
-            Shadow(
-              color: Colors.black,
-              blurRadius: 2,
-              offset: Offset(2, 2),
-            ),
-          ],
-        ),
+        title: Text(
+          "CHECADOR",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Montserrat',
+            color: Colors.white,
+            letterSpacing: 2.0,
+            shadows: [
+              Shadow(
+                color: Colors.black,
+                blurRadius: 2,
+                offset: Offset(2, 2),
+              ),
+            ],
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.green,
@@ -41,23 +57,65 @@ class _MyAppState extends State<MyApp> {
       body: dinamico(),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.menu_book_sharp), label: "Materia" , backgroundColor: Colors.green),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: "Profesor" , backgroundColor: Colors.yellowAccent.shade700),
-          BottomNavigationBarItem(icon: Icon(Icons.access_time_filled), label: "Horario" , backgroundColor: Colors.red),
-          BottomNavigationBarItem(icon: Icon(Icons.format_list_numbered), label: "Asistencia" , backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_sharp),
+              label: "Materia",
+              backgroundColor: Colors.green),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: "Profesor",
+              backgroundColor: Colors.yellowAccent.shade700),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_time_filled),
+              label: "Horario",
+              backgroundColor: Colors.red),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.format_list_numbered),
+              label: "Asistencia",
+              backgroundColor: Colors.blue),
         ],
         currentIndex: _indice,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
-            _indice=index;
-
+            _indice = index;
           });
         },
         unselectedItemColor: Colors.black,
         selectedItemColor: Colors.white,
       ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          onPressed: () {
+            GUIMateria.formularioRegistrar(this, true);
+          },
+          child: Row(
+            children: [
+              Icon(Icons.menu_book, color: Colors.green.shade50),
+              Icon(
+                Icons.add,
+                color: Colors.green.shade50,
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          )),
     );
   }
 
-  dinamico() {}
+  dinamico() {
+    switch (_indice) {
+      default:
+        return GUIMateria.listaMaterias(this);
+    }
+  }
+
+  void consultarMaterias() async {
+    var x = await DBMateria.consultar();
+    setState(() {
+      materias = x;
+    });
+  }
+
+  void mensaje(String s) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(s)));
+  }
 }
